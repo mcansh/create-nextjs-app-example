ed7888debcdcc6827084d70ea3de566e98d6789f
Logan McAnsh <logan@mcan.sh>
2018-07-29 16:55:12 -0400

    update example
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Hello.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Hello = () => (
-  <h1>create-nextjs-app-example</h1>
-);
-
-export default Hello;
--- readme.markdown
+++
@@ -1,1 +0,0 @@
-a sample app that was generated with [create-nextjs-app](https://github.com/mcansh/create-nextjs-app)
